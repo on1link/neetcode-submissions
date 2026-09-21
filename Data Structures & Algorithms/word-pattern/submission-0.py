edacf74d36b1ee149cf24de236cdd3a58115ec05
Jorge Nacer <jorge.nacerc@gmail.com>
2026-09-21 17:54:17 +0200

    Add: word-pattern - submission-0

---
+++ Data Structures & Algorithms/word-pattern/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        charToWord = {}
+        store = set()
+        
+        for i, (c, w) in enumerate(zip(pattern, words)):
+            if c in charToWord:
+                if words[charToWord[c]] != w:
+                    return False
+            else:
+                if w in store:
+                    return False
+                charToWord[c] = i
+                store.add(w)
+
+        return True
